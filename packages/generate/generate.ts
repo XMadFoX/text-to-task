2b327e04080128a60350e1c20125b0c361da879f
MadFox <madfox@laserflare.net>
2024-12-06 15:14:00 +0400

    refactor(generate): use ai sdk to generate text

--- packages/generate/generate.ts
+++ packages/generate/generate.ts
@@ -1,20 +1,34 @@
-import { generate } from './openai';
+import { openaiModel } from './openai';
 import { systemPrompt } from './template';
+import { generateText } from 'ai';
 
 export type expctedTaskJson = {
-  title: string;
-  description: string;
-  statedId: string;
-  assigneeId?: string;
-  projectId?: string;
-  labels?: string[];
-  priority?: number; // 0 = No priority, 1 = Urgent, 2 = High, 3 = Normal, 4 = Low
+	title: string;
+	description: string;
+	statedId: string;
+	assigneeId?: string;
+	projectId?: string;
+	labels?: string[];
+	priority?: number; // 0 = No priority, 1 = Urgent, 2 = High, 3 = Normal, 4 = Low
 };
 
 export async function generateTask(msg: string, ctx: string) {
-  const prompt = `${systemPrompt}\n${ctx}`;
-  console.log(prompt);
+	const prompt = `${systemPrompt}\n${ctx}`;
+	console.debug('System prompt', prompt);
 
-  const json = await generate(prompt, msg);
-  return json;
+	const { text } = await generateText({
+		model: openaiModel,
+		system: prompt,
+		prompt: msg,
+	});
+
+	const json = await parseAsync(text).catch((e) => {
+		throw new Error('Failed to parse JSON from OpenAI');
+	});
+
+	return json;
+}
+
+async function parseAsync(a: string) {
+	return JSON.parse(a) as expctedTaskJson;
 }
--- packages/generate/openai/index.ts
+++ packages/generate/openai/index.ts
@@ -1,33 +1,8 @@
-import OpenAI from 'openai';
-import { expctedTaskJson } from '../generate.ts';
+import { createOpenAI } from '@ai-sdk/openai';
 import { openaiEnv } from './env';
 
-const openai = new OpenAI({
+const openai = createOpenAI({
 	apiKey: openaiEnv.OPENAI_API_KEY,
 });
 
-export async function generate(systemPrompt: string, userPrompt: string) {
-	const chatCompletion = await openai.chat.completions.create({
-		messages: [
-			{ role: 'system', content: systemPrompt },
-			{
-				role: 'user',
-				content: userPrompt,
-			},
-		],
-		model: openaiEnv.OPENAI_MODEL,
-	});
-
-	const output = chatCompletion.choices[0].message.content;
-	if (!output) throw new Error('No output from OpenAI');
-
-	const json = await parseAsync(output).catch((e) => {
-		throw new Error('Failed to parse JSON from OpenAI');
-	});
-
-	return json;
-}
-
-async function parseAsync(a: string) {
-	return JSON.parse(a) as expctedTaskJson;
-}
+export const openaiModel = openai(openaiEnv.OPENAI_MODEL);
