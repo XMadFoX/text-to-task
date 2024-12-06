7d27adb9a59ab91b6426bf306a219e8b53998cf5
MadFox <madfox@laserflare.net>
2024-12-06 16:14:58 +0400

    feat(generate): add base url env support

--- packages/generate/models/anthropic.ts
+++ packages/generate/models/anthropic.ts
@@ -3,6 +3,7 @@ import { env } from '../env';
 
 const anthropic = createAnthropic({
 	apiKey: env.LLM_API_KEY,
+	baseURL: env.LLM_BASE_URL,
 });
 
 export const anthropicModel = anthropic(env.LLM_MODEL);
--- packages/generate/models/openai.ts
+++ packages/generate/models/openai.ts
@@ -3,6 +3,7 @@ import { env } from '../env';
 
 const openai = createOpenAI({
 	apiKey: env.LLM_API_KEY,
+	baseURL: env.LLM_BASE_URL,
 });
 
 export const openaiModel = openai(env.LLM_MODEL);
